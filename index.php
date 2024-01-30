235152a408771dc0358336827cc37edc9a84e535
igorgaidolfi <gandalf.4@outlook.it>
2024-01-30 16:39:43 +0100

    added movies

--- index.php
+++ index.php
@@ -1,12 +1,28 @@
 <?php 
-    class Movies{
+    class Movie{
         public $title;
         public $year;
         public $genre;
         public $director;
+        public $duration;
+
+        function __construct($_title, $_year, $_genre){
+            $this->title = $_title;
+            $this->year = $_year;
+            $this->genre = $_genre;
+        }
     }
 
+    $movies_1 = new Movie("Il Padrino", 1972, "Noir");
+    $movies_2 = new Movie("Il cavaliere oscuro", 2009, "Azione");
+    $movies_3 = new Movie("Figth Club", 1999, "Drammatico");
+    $movies_4 = new Movie("Quei bravi ragazzi", 1990, "Crime");
+    $movies_5 = new Movie("Pulp Fiction", 1994, "Thriller");
+    $movies_6 = new Movie("Il Signore degli Anelli: Il Ritorno del Re", 2003, "Fantasy");
+    $movies_7 = new Movie("Inception", 2010, "Fantascienza");
+    $movies_7 = new Movie("Il pianista", 2010, "Guerra");
 
+    
 ?>
 
 <!DOCTYPE html>
